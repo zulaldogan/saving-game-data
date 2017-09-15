d86ffa2433043f7987a271f13f2e15d3d0be1ffe
ZÜLAL DOĞAN <zulaldogan@users.noreply.github.com>
2017-09-15 12:43:27 +0300

    PlayerPrefsExample
    https://www.youtube.com/watch?v=BgxbCej0GOg&t=432s&list=PL5KbKbJ6Gf9_Rvhx1ye1iHxtLTsAaH7uD&index=2

---
+++ PlayerPrefsExample/Assets/Test.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    private string key = "MyFirstKey";
+    public int value;
+
+    void Start()
+    {
+        PlayerPrefs.SetInt(key,value);
+       
+        Debug.Log(PlayerPrefs.GetInt(key).ToString());
+    }
+}
